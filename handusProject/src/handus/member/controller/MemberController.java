eb00506b9f1545291f2fb12260fefd21273eaba1
lkjhnm <cdc9308@naver.com>
2019-08-27 14:12:24 +0900

    헤더 수정

--- handusProject/src/handus/member/controller/MemberController.java
+++ handusProject/src/handus/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package handus.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import handus.dao.MemberDao;
 import handus.member.service.IMemberService;
@@ -13,6 +14,15 @@ public class MemberController {
 	@Autowired
 	private IMemberService memberService;
 	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String handusLogin() {
+		return "login";
+	}
+	//로그인 포스트 작업 아직안함
 	
-	
+	@RequestMapping(value="/signUp",method=RequestMethod.GET)
+	public String handusSignUp() {
+		
+		return "signUp";
+	}
 }
