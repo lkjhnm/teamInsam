6b7882593ce04a9df6c21974f9a2ded63c77b761
lkjhnm <49580115+lkjhnm@users.noreply.github.com>
2019-09-30 16:53:18 +0900

    키보안
    보안

--- handusProject/src/handus/image/service/S3App.java
+++ handusProject/src/handus/image/service/S3App.java
@@ -14,9 +14,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class S3App {
 	
 	public static final AWSCredentials credentials;
-	private static final String KEY_ID="AKIAIXZPLZE2IACKQ5AQ";
+	private static final String KEY_ID="";
 	
-	private static final String SECRET_ACCESS_KEY = "F8EvrzYAv2eLU2OfKBDe7N0VVx5sUWd4B1v+JIn+";
+	private static final String SECRET_ACCESS_KEY = "";
 	
 	static {
 		credentials = new BasicAWSCredentials(KEY_ID, SECRET_ACCESS_KEY);
